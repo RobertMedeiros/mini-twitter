215db7b2f0e6622fd1a2fc1fff2c5df8e3c97431
RobertMedeiros <robertnobrega14@gmail.com>
2026-03-21 11:59:09 -0300

    mudanças na font, titulo e favicon

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -2,8 +2,8 @@
 
 export default function Footer() {
     return (
-        <footer className="bg-[#e7f3f9] dark:bg-slate-950 text-right p-3 transition-colors">
-            <p className="text-[#1ba0f3] uppercase tracking-tighter">Mini Twitter</p>
+        <footer className="bg-[#e7f3f9] dark:bg-slate-950 text-left p-3 transition-colors">
+            <p className="text-[#1ba0f3] uppercase tracking-tighter">Mini Twitter</p>   
         </footer>
     )
 }
